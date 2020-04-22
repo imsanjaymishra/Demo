833c669a155f98a8f95e9c483bcd491b16a8e6a5
Sanjay Mishra <jaysan.mishra@gmail.com>
2020-04-22 15:31:47 +0530

    New VC Added

---
+++ Demo2/NewVCViewController.swift
@@ -0,0 +1,31 @@
+//
+//  NewVCViewController.swift
+//  Demo2
+//
+//  Created by Sanjay Mishra on 22/04/20.
+//  Copyright © 2020 Sanjay Mishra. All rights reserved.
+//
+
+import UIKit
+
+class NewVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("New VC Added")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
